c2844431e65a67356fcc5f179f4041e950cc22a4
Alex Bertran <70992408+ApBertran@users.noreply.github.com>
2022-11-28 14:05:14 -0800

    Create 2-2015.py

---
+++ Practice/2-2015.py
@@ -0,0 +1,33 @@
+textFile = open('2-2015-text.txt', 'r')
+textLines = textFile.readlines()
+
+count = 0
+totalSurfaceArea = 0
+
+for line in textLines:
+    chrctrCount = 0
+    lastKnown = 0
+    xCount = 0
+    l = 0
+    w = 0
+    for chrctr in line:
+        chrctrCount += 1
+        if chrctr == 'x':
+            if xCount == 0:
+                l = int(line[0:chrctrCount - 1])
+                lastKnown = chrctrCount
+            elif xCount == 1:
+                w = int(line[lastKnown:chrctrCount - 1])
+                lastKnown = chrctrCount
+            xCount += 1
+    h = int(line[lastKnown:chrctrCount - 1])
+    
+    sides = list()
+    sides.append(int(2 * l * w))
+    sides.append(int(2 * w * h))
+    sides.append(int(2 * l * h))
+    smallestSide = int(min(sides) / 2)
+    surfaceArea = sides[0] + sides[1] + sides[2] + smallestSide
+    totalSurfaceArea += surfaceArea
+
+print(totalSurfaceArea)
